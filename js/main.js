de6be30b857253c9bbc9713730818335b52ab2f1
Sofia <sofiaxaguirre@gmail.com>
2018-07-29 16:36:33 -0700

    created game logic, updated with objects and functions, needs to fix alerts

--- js/main.js
+++ js/main.js
@@ -1,4 +1,25 @@
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+  {
+    rank: "queen",
+    suit: "hearts",
+    cardImage: "images/queen-of-hearts.png",
+  },
+  {
+    rank: "queen",
+    suit: "diamonds",
+    cardImage: "images/queen-of-diamonds.png",
+  },
+  {
+    rank: "king",
+    suit: "hearts",
+    cardImage: "images/king-of-hearts.png",
+  },
+  {
+    rank: "king",
+    suit: "diamonds",
+    cardImage: "images/king-of-diamonds.png",
+  },
+];
 var cardsInPlay = [];
 
 var checkForMatch = function() {
@@ -12,13 +33,20 @@ var checkForMatch = function() {
 
 var flipCard = function(cardId) {
   checkForMatch();
-  console.log("You flipped: " + cards[cardId]);
+  console.log("You flipped: " + cards[cardId].rank);
+  cardsInPlay.push(cards[cardId].rank);
+  console.log(cards[cardId].cardImage);
+  console.log(cards[cardId].suit);
 };
 
-//flipCard(0);
-//flipCard(2);
+flipCard(0);
+flipCard(2);
 
-cardsInPlay.push(cards[cardId]);
+// cardsInPlay here from line 37
+
+//copied into flipCard at lines 38, 39
+//console.log(cards[cardId].cardImage);
+//console.log(cards[cardId].suit);
 
 
 /* This used to sit inside the flipCard function. Hopefully I didn't break anything:
